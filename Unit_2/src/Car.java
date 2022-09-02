23c8f8e0a66fe80a8a5f2360dbf78169ee471961
FarhanSadeek21 <farhansadeek019@gmail.com>
2022-09-02 13:43:23 -0400

    Add String

--- Unit_2/src/Car.java
+++ Unit_2/src/Car.java
@@ -2,6 +2,7 @@ public class Car {
     public String make;
     public String owner;
     public boolean isSports;
+    public int year;
 
     // Blank constructor
 
@@ -9,11 +10,12 @@ public class Car {
         make = "";
         owner = "";
         isSports = false;
+        year = Integer.MIN_VALUE;
     }
 
     // Constructors with parameters
 
-    public Car(String make, String owner, boolean isSports){
+    public Car(String make, String owner, boolean isSports, int year){
         this.make = make;
         this.owner = make;
         this.isSports = isSports;
--- Unit_2/src/Main.java
+++ Unit_2/src/Main.java
@@ -2,10 +2,17 @@ import java.util.*;
 public class Main {
     public static void main(String [] args){
         Scanner inp = new Scanner(System.in);
+        System.out.print("Make of the car : ");
         String make = inp.next();
+        System.out.print("Owner of the car : ");
         String owner = inp.next();
+        System.out.print("Sports car : ");
         boolean isSports = inp.nextBoolean();
-        Car car1 = new Car(make, owner, isSports);
+        System.out.print("Year of manufacture : ");
+        int year = inp.nextInt();
+        Car car1 = new Car(make, owner, isSports, year);
         System.out.println(car1.owner);
+        System.out.println(car1.year);
+        System.out.println(car1.isSports);
     }
 }
