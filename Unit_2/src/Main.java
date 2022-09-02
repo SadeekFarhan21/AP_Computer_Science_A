afbce4d61b4b470c84b143b2cb875420a4740324
FarhanSadeek21 <farhansadeek019@gmail.com>
2022-09-02 13:58:17 -0400

    Update

--- Unit_2/src/Main.java
+++ Unit_2/src/Main.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class Main {
     public static void main(String [] args){
-        Scanner inp = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Make of the car : ");
-        String make = inp.next();
+        String make = input.next();
         System.out.print("Owner of the car : ");
-        String owner = inp.next();
+        String owner = input.next();
         System.out.print("Sports car : ");
-        boolean isSports = inp.nextBoolean();
+        boolean isSports = input.nextBoolean();
         System.out.print("Year of manufacture : ");
-        int year = inp.nextInt();
+        int year = input.nextInt();
         Car car1 = new Car(make, owner, isSports, year);
         System.out.println(car1.owner);
         System.out.println(car1.year);
