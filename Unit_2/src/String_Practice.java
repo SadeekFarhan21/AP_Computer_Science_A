2c231946aed1d08c92d861d407c62ea2d2511d7f
farhande110 <farhansadeek019@gmail.com>
2022-09-16 11:17:18 -0400

    String Practice class and java program

---
+++ Unit_2/src/String_Practice.java
@@ -0,0 +1,12 @@
+// import java.util.*;
+class String_Practice {
+    public static void main(String[] args){
+        String a = new String("Farhan is a stupid person.");
+        System.out.println(a.contains("i"));
+
+        String favClass = "Beriod 4";
+        String p4 = "Class 4";
+        System.out.println(favClass.compareTo(p4));
+        System.out.println(p4.compareTo(favClass));
+    }
+}
