3bb6a23a8d5ea8c2ecd8c720aa7f3456deab124d
farhande110 <farhansadeek019@gmail.com>
2022-09-22 18:40:24 -0400

    Unit 2 Notes

--- Unit_2/src/String_.java
+++ Unit_2/src/String_.java
@@ -1,8 +1,9 @@
 public class String_{
     public static void main(String [] args){
         // Compare To returns how far is the first from the second towards the positive direction
-        // if 0 then equals
+        // if 0 then both are equal
         // if negative then the second one is later than the first one chronologically
+        // if positive then the first one is later than the second one chronologically
         // CompareTo = Comparable String - Actual String
         // S(19) - F(6) = 13 ; Second one is ahead means positive
         // F(6) - S(19) = -13; First one is ahead means negative
