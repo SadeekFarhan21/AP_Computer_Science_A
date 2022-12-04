0668b95b00ff1b8396e043346a17a44fd01e509a
Farhan Sadeek <farhansadeek019@gmail.com>
2022-12-04 00:48:20 -0500

    Daily Vidoes 2 Part 3

--- Unit_5/src/Cow.java
+++ Unit_5/src/Cow.java
@@ -1,3 +1,27 @@
 public class Cow {
-    
+    private String type;
+    private String sound;
+    private int numOfMilkings;
+    public Cow(String type, String sound){
+        this.type = type;
+        this.sound = sound;
+        numOfMilkings = 0;
+    }
+
+    public String getSound(){
+        return sound;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void milkCow(){
+        numOfMilkings += 1;
+    }
+
+    public int getNumMilking(){
+        return numOfMilkings;
+    }
+
 }
