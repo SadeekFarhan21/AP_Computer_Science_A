e6fc9a23b4df7831bd63ea5ece8cdd6d20b94340
Farhan Sadeek <farhansadeek019@gmail.com>
2022-12-03 21:57:18 -0500

    Daily Video 1.1 Part 3

---
+++ Unit_5/src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String title;
+    private String genere;
+    private String author;
+    private int pages;
+    
+    public Book(){
+        // default constructor
+    }
+    
+}
