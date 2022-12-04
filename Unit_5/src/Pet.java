abe194a45bd93db9845aa280b26f0ba3ccdb97b2
Farhan Sadeek <farhansadeek019@gmail.com>
2022-12-04 01:18:39 -0500

    Daily Vidoes 4 Part 1

---
+++ Unit_5/src/Pet.java
@@ -0,0 +1,38 @@
+// Accessor methods
+// returns a value
+// known as get method
+// no parameters
+// must be public
+
+public class Pet{
+    private String name;
+    private String typeOfPet;
+    private int age;
+    
+    // default constructor 
+    public Pet(){
+        name = "Default name";
+        typeOfPet = "Default type";
+        age = 0;
+    }   
+    
+    // overloaded constructor
+    public Pet(String name, String typeOfPet, int age){
+        this.name = name;
+        this.typeOfPet = typeOfPet;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getTypeOfPet(){
+        return typeOfPet;
+    }
+
+    public int age(){
+        return age;
+    }
+
+}
