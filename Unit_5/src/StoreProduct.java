82c7e5dd3207acee57fcf83bcd2861c1c3082afa
Farhan Sadeek <farhansadeek019@gmail.com>
2022-12-04 22:22:33 -0500

    Store Prodcut

--- Unit_5/src/StoreProduct.java
+++ Unit_5/src/StoreProduct.java
@@ -1,3 +1,5 @@
+import javax.print.attribute.HashPrintJobAttributeSet;
+
 // import java.util.*;
 public class StoreProduct {
     // Variables
@@ -19,6 +21,8 @@ public class StoreProduct {
         // Default values for 
         category = "misc";
         hasExpiration = false;
+        label = "Default label";
+        price = 0;
     }
 
     // Secondary Constructor
@@ -27,6 +31,17 @@ public class StoreProduct {
     public StoreProduct(String label, int price){
         this.label = label;
         this.price = price;
+        category = "Default category";
+        hasExpiration = false;
+        stock = 0;
+    }
+
+    public StoreProduct(String label, int price, String cateogry, int stock, boolean hasExpiration){
+        this.label = label;
+        this.price = price;
+        this.category = cateogry;
+        this.stock = stock;
+        this.hasExpiration = hasExpiration;
     }
 
 }
